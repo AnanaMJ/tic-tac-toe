77f5598bc1c293086e554f30d4a2930c9aea2ab0
Ana Maria Jalba <ana.jalba13@gmail.com>
2020-03-04 15:14:18 +0000

    Use hooks instead of this.state

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
-import './App.css';
-import { GameControls } from '../GameControls/GameControls';
-import Board, { SquareState } from '../Board/Board';
-import { isUndefined, isNullOrUndefined } from 'util';
-import { calculateWinner, getWinnerLine } from '../Functions/Functions';
+import React, { useState } from "react";
+import "./App.css";
+import { GameControls } from "../GameControls/GameControls";
+import Board, { SquareState } from "../Board/Board";
+import { isNullOrUndefined } from "util";
+import { calculateWinner, getWinnerLine } from "../Functions/Functions";
 
 export interface BoardState {
-  boardState: Array<SquareState>,
-  latestMove: number,
+  boardState: Array<SquareState>;
+  latestMove: number;
 }
 
 export interface AppState {
@@ -19,133 +19,137 @@ export interface AppState {
   isAscending: boolean;
 }
 
-class App extends React.Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props);
-    this.reset();
-  }
-
-  reset = () => {
-    const history = [{
+const App: React.FC<{}> = () => {
+  const [history, setHistory] = useState<Array<BoardState>>([
+    {
       boardState: Array(9).fill(null),
       latestMove: 0
-    }];
-
-
-    const state = {
-      history,
-      stepNumber: 0,
-      winner: null,
-      winnerLine: undefined,
-      xIsNext: true,
-      isAscending: true,
-    };
-
-    if (isUndefined(this.state)) {
-      this.state = state;
-    } else {
-      this.setState(state);
     }
-  }
+  ]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [winner, setWinner] = useState<SquareState>(null);
+  const [winnerLine, setWinnerLine] = useState<Array<number> | undefined>(
+    undefined
+  );
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+  const [isAscending, setIsAscending] = useState<boolean>(true);
 
-  handleBoardClick = (i: number) => {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
+  const reset = () => {
+    setHistory([
+      {
+        boardState: Array(9).fill(null),
+        latestMove: 0
+      }
+    ]);
+    setStepNumber(0);
+    setWinner(null);
+    setWinnerLine(undefined);
+    setXIsNext(true);
+    setIsAscending(true);
+  };
+
+  const handleBoardClick = (i: number) => {
+    const currentHistory = history.slice(0, stepNumber + 1);
+    const current = currentHistory[currentHistory.length - 1];
     const boardState = current.boardState.slice() as Array<SquareState>;
-    // const latestMove = current.latestMove;
 
-    if (!isNullOrUndefined(boardState[i]) || this.state.winner) {
+    if (!isNullOrUndefined(boardState[i]) || winner) {
       return;
     }
 
-    boardState[i] = this.state.xIsNext ? 'X' : 'O';
-    const winner = calculateWinner(boardState);
-    const winnerLine = getWinnerLine(boardState);
+    boardState[i] = xIsNext ? "X" : "O";
+    const currentWinner = calculateWinner(boardState);
+    const currentWinnerLine = getWinnerLine(boardState);
 
-    this.setState({
-      history: history.concat([{boardState:boardState, latestMove: i}]),
-      stepNumber: history.length,
-      winner,
-      winnerLine,
-      xIsNext: !this.state.xIsNext,
-    })
-  }
+    setHistory(
+      currentHistory.concat([{ boardState: boardState, latestMove: i }])
+    );
+    setStepNumber(currentHistory.length);
+    setWinner(currentWinner);
+    setWinnerLine(currentWinnerLine);
+    setXIsNext(!xIsNext);
+  };
 
-  handleSortToggle() {
-    this.setState({
-      isAscending: !this.state.isAscending
-    });
-  }
+  const handleSortToggle = () => {
+    setIsAscending(!isAscending);
+  };
 
-  jumpTo(step: number) {
-    if (!this.state.winner) {
-      this.setState({
-        stepNumber: step,
-        xIsNext: (step % 2) === 0 ? false : true,
-      })
+  const jumpTo = (step: number) => {
+    if (!winner) {
+      setStepNumber(step);
+      setXIsNext(step % 2 === 0 ? false : true);
     }
-  }
+  };
 
-  getStatusText() {
-    if (this.state.winner) {
-      return `Winner: ${this.state.winner}`;
-    } else if (this.state.stepNumber >= 9) {
+  const getStatusText = () => {
+    if (winner) {
+      return `Winner: ${winner}`;
+    } else if (stepNumber >= 9) {
       return "It's a tie!";
     }
 
-    return `Current turn: ${this.state.xIsNext ? 'X' : 'O'}`;
-  }
+    return `Current turn: ${xIsNext ? "X" : "O"}`;
+  };
 
-  getPosition(i: number) {
-    const positions = [[0,0], [0,1], [0,2], [1,0], [1,1], [1,2], [2,0], [2,1], [2,2]];
+  const getPosition = (i: number) => {
+    const positions = [
+      [0, 0],
+      [0, 1],
+      [0, 2],
+      [1, 0],
+      [1, 1],
+      [1, 2],
+      [2, 0],
+      [2, 1],
+      [2, 2]
+    ];
     return positions[i];
-  }
+  };
 
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const status = this.getStatusText();
-    const moves = history.map((step, move) => {
-      const position = this.getPosition(step.latestMove);
-      const desc = move ? `Go to move #${move} (${position[0]}, ${position[1]})` : 'Go to game start';
-      const bold = move === this.state.stepNumber ? 'bold' : '';
-
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)} className={bold}>{desc}</button>
-        </li>
-      )
-    });
-
-    const isAscending = this.state.isAscending;
-    if (!isAscending) {
-      moves.reverse();
-    }
+  const current = history[stepNumber];
+  const status = getStatusText();
+  const moves = history.map((step, move) => {
+    const position = getPosition(step.latestMove);
+    const desc = move
+      ? `Go to move #${move} (${position[0]}, ${position[1]})`
+      : "Go to game start";
+    const bold = move === stepNumber ? "bold" : "";
 
     return (
-      <div className="App">
+      <li key={move}>
+        <button onClick={() => jumpTo(move)} className={bold}>
+          {desc}
+        </button>
+      </li>
+    );
+  });
 
-        <div className="Board">
-          <Board
-            squares={current.boardState}
-            winnerLine={this.state.winnerLine}
-            onClick={this.handleBoardClick}
-          />
-        </div>
+  if (!isAscending) {
+    moves.reverse();
+  }
 
-        <GameControls
-          reset={this.reset}
+  return (
+    <div className="App">
+      <div className="Board">
+        <Board
+          squares={current.boardState}
+          winnerLine={winnerLine}
+          onClick={handleBoardClick}
         />
-        <div className="game-info">
-          <div className="text-center">{status}</div>
-          <button onClick={()=> this.handleSortToggle()}>
-            {isAscending ? 'Set to descending order' : 'Set to ascending order'}
-          </button>
-          <div><ol>{moves}</ol></div>
+      </div>
+
+      <GameControls reset={reset} />
+      <div className="game-info">
+        <div className="text-center">{status}</div>
+        <button onClick={() => handleSortToggle()}>
+          {isAscending ? "Set to descending order" : "Set to ascending order"}
+        </button>
+        <div>
+          <ol>{moves}</ol>
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
 export default App;
